9cb52d6f13233c60c455ad77e62db03ac1688613
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-01-23 10:32:35 +0100

    first cut at OQS-HAproxy-only appliance

--- haproxy/scripts/dockerbuild.sh
+++ haproxy/scripts/dockerbuild.sh
@@ -8,3 +8,12 @@ if [ $# -gt 0 ]; then
    echo "Pushing image to openqsafe. Be sure to be logged in, eg via 'docker login -u openqsafe'"
    docker tag $BASENAME openqsafe/$BASENAME && docker push openqsafe/$BASENAME
 fi
+
+rm -rf opt
+mkdir opt
+# Export installed dirs
+docker run -v `pwd`/opt:/home/opt -t $BASENAME bash -c "cp -R /opt/* /home/opt"
+
+rm -rf opt/bin/curl /opt/lib/*curl*
+
+docker build -t $BASENAME-appliance -f Dockerfile-appliance .
