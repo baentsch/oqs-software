d8c704041bec496de549f827655e7735a5df3fc5
Michael Baentsch <57787676+baentsch@users.noreply.github.com>
2020-02-10 12:24:10 +0100

    ENV VAR corrected

--- haproxy/scripts/sign-server-csr.sh
+++ haproxy/scripts/sign-server-csr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Mount both root CA and haproxy folders to generate haproxy server certificate:
-docker run -v `pwd`/oqs-root:/opt/haproxy/root -v `pwd`/oqs-haproxy:/opt/haproxy/server --entrypoint /opt/haproxy/sh.sh -it haproxy-alpine openssl $OPENSSL x509 -req -in /opt/haproxy/server/server.csr -out /opt/haproxy/server/server.crt -CA /opt/haproxy/root/CA.crt -CAkey /opt/haproxy/root/CA.key -CAcreateserial -days 365
+docker run -v `pwd`/oqs-root:/opt/haproxy/root -v `pwd`/oqs-haproxy:/opt/haproxy/server --entrypoint /opt/haproxy/sh.sh -it haproxy-alpine openssl x509 -req -in /opt/haproxy/server/server.csr -out /opt/haproxy/server/server.crt -CA /opt/haproxy/root/CA.crt -CAkey /opt/haproxy/root/CA.key -CAcreateserial -days 365
 
